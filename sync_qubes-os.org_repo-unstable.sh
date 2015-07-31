96b6e1eb8e221cec5d991a1d871ef52bbae8ebb2
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2015-07-31 22:38:34 +0200

    Update scripts for new basedir on the server
    Signed-off-by: Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>

--- sync_qubes-os.org_repo-unstable.sh
+++ sync_qubes-os.org_repo-unstable.sh
@@ -4,7 +4,8 @@ pushd `dirname $0`
 
 #DRY="-n"
 USERNAME=marmarek
-HOST=yum.qubes-os.org
+HOST=deb.qubes-os.org
+HOST_BASEDIR=/pub/qubes/repo/yum
 RELS_TO_SYNC="r2 r3 r3.0"
 if [ -n "$1" ]; then
     RELS_TO_SYNC="$1"
@@ -13,11 +14,11 @@ REPOS_TO_SYNC="jessie-unstable wheezy-unstable"
 
 for rel in $RELS_TO_SYNC; do
     rsync $DRY --partial --progress --hard-links -air $rel/vm/pool \
-          $USERNAME@$HOST:/var/www/deb.qubes-os.org/$rel/vm/
+          $USERNAME@$HOST:$HOST_BASEDIR/$rel/vm/
     for repo in $REPOS_TO_SYNC; do
         echo "Syncing $rel/vm/$repo..."
         rsync $DRY --partial --progress --hard-links -air $rel/vm/dists/$repo \
-            $USERNAME@$HOST:/var/www/deb.qubes-os.org/$rel/vm/dists/
+            $USERNAME@$HOST:$HOST_BASEDIR/$rel/vm/dists/
     done
 
 done
