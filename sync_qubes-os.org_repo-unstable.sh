8d24a5d5b5c6cde73d0caa1a4d66e23a5766e8a8
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2017-04-02 18:48:39 +0200

    Drop explicit ssh username from scripts
    If needed, can be configured using ~/.ssh/config.

--- sync_qubes-os.org_repo-unstable.sh
+++ sync_qubes-os.org_repo-unstable.sh
@@ -3,7 +3,6 @@
 pushd `dirname $0`
 
 #DRY="-n"
-USERNAME=marmarek
 HOST=deb.qubes-os.org
 HOST_BASEDIR=/pub/qubes/repo/deb
 RELS_TO_SYNC="r2 r3.0"
@@ -14,11 +13,11 @@ REPOS_TO_SYNC="`for r in $RELS_TO_SYNC; do ls $r/vm/dists | grep unstable; done`
 
 for rel in $RELS_TO_SYNC; do
     rsync $DRY --partial --progress --hard-links -air $rel/vm/pool \
-          $USERNAME@$HOST:$HOST_BASEDIR/$rel/vm/
+          $HOST:$HOST_BASEDIR/$rel/vm/
     for repo in $REPOS_TO_SYNC; do
         echo "Syncing $rel/vm/$repo..."
         rsync $DRY --partial --progress --hard-links -air $rel/vm/dists/$repo \
-            $USERNAME@$HOST:$HOST_BASEDIR/$rel/vm/dists/
+            $HOST:$HOST_BASEDIR/$rel/vm/dists/
     done
 
 done
